5a85d8f98d487d29e8eb193cb13b3fe160d498f7
vitalya.efimov <vitalay.efimov@gmail.com>
2024-11-10 20:56:13 +0500

    добавили Input

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,7 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Привет"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import MayHello from "./components/MayHello.vue"
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <MayHello />
+</template>
---
+++ src/components/MayHello.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+
+function increment() {
+    count.value++
+}
+
+function updateCount(e) {
+    count.value = Number(e.target.value)
+}
+
+// export default {
+//     data() {
+//         return {
+//             count: 0
+//         }
+//     },
+//     methods: {
+//         increment() {
+//             this.count++
+//         }
+//     }
+// }
+
+</script>
+
+
+<template>
+  <h1>{{count}}</H1>
+  <button @click="increment">+</button>
+  <input @input="updateCount" />
+</template>
+
